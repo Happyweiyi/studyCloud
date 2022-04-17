0e609c92800e3aeea5b7eb80cc3038aabde0c71b
Happyweiyi <923351994@qq.com>
2022-04-17 21:53:16 +0800

    5.Discovery服务发现   查看相关服务配置信息

--- paymentCloud1/src/main/java/com/hewei/springcloud/controller/PaymentController.java
+++ paymentCloud1/src/main/java/com/hewei/springcloud/controller/PaymentController.java
@@ -6,9 +6,12 @@ import com.hewei.springcloud.entities.Payment;
 import com.hewei.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -20,6 +23,9 @@ public class PaymentController {
     //读取application.yml的端口号
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -41,5 +47,22 @@ public class PaymentController {
         }else {
             return new CommonResult(500,"查询数据失败,查询id:"+id,null);
         }
+    }
+
+    @GetMapping(value="/payment/discovery")
+    public Object discovery(){
+        //查询全部的服务信息
+        List<String> services = discoveryClient.getServices();
+        for(String element:services){
+            log.info("****element:"+element);
+        }
+
+        //查询服务的具体信息，例如主机ip,端口号,url
+        List<ServiceInstance> instances = discoveryClient.getInstances("PAYMENT-SERVICE");
+        for(ServiceInstance instance : instances){
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 }
